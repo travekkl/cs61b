0a8e36ec154ab7aa8c6b8e83d7748fe5725194f0
travekkl <1976545289@qq.com>
2020-08-22 18:45:44 +0800

    completed project0 20200822

--- proj1b/PalindromeFinder.java
+++ proj1b/PalindromeFinder.java
@@ -2,20 +2,20 @@
 public class PalindromeFinder {
 
     public static void main(String[] args) {
-        int minLength = 4;
+        /*int minLength = 4;
         In in = new In("../library-sp18/data/words.txt");
         Palindrome palindrome = new Palindrome();
         OffByN cc = new OffByN(5);
         while (!in.isEmpty()) {
             String word = in.readString();
-            /*if (word.length() >= minLength && palindrome.isPalindrome(word)) {
+            if (word.length() >= minLength && palindrome.isPalindrome(word)) {
                 System.out.println(word);
-            }*/
+            }
 
             if (word.length() >= minLength && palindrome.isPalindrome(word, cc)) {
                 System.out.println(word);
             }
-        }
+        }*/
 
     }
 }
